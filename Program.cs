9a8eafd95e9a85ed789f888d6cd13be510dda0ca
lauren maxwell <laurenelizamax@icloud.com>
2019-11-17 09:48:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>()
+            {
+                 "Mercury", "Mars"
+                  };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> newPlanets = new List<string>() {
+                "Jupiter", "Saturn"
+                    };
+
+            planetList.AddRange(newPlanets);
+            planetList.Insert(0, "Earth");
+            planetList.Insert(5, "Venus");
+            planetList.Add("Pluto");
+            planetList.Remove("Pluto");
+
+
+            planetList.ForEach(i => Console.Write("{0}\t", i));
+
+            // Console.WriteLine(planetList[1]);
+
+        }
+    }
+}
+
